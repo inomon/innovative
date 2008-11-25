9f466e1f305f97627c73ef14b622fc09031d49d4
ino110987 <oturan_fan@yahoo.com>
2008-11-25 15:21:43 +0800

    added a .htaccess file, revised application specific reciever [index.php, dev.php, etc]

--- dev.php
+++ dev.php
@@ -1,6 +1,9 @@
 <?php
 define('DIR_SEP', '/');
-define('LIB',     realpath(dirname(__FILE__)));
-define('ENV',     'dev');
+define('LIB',     realpath(dirname(__FILE__).DIR_SEP.'inno'));
+define('APPLI',   'dev');
 
 require_once(LIB.DIR_SEP.'conf'.DIR_SEP.'config.php');
+
+$controller = new innoController();
+$controller->run();
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 define('DIR_SEP', '/');
-define('LIB',     realpath(dirname(__FILE__).DIR_SEP.'phpcls'));
-define('ENV',     'fro');
+define('LIB',     realpath(dirname(__FILE__).DIR_SEP.'inno'));
+define('APPLI',   'fro');
 
 require_once(LIB.DIR_SEP.'conf'.DIR_SEP.'config.php');
+
+$controller = new innoController();
+$controller->run();
